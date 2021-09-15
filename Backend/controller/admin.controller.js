d4c347b0da657f452e0ace8ebdb78579ef5dedb7
sathwika-katam <katam.s@husky.neu.edu>
2021-09-15 15:46:26 -0700

    admin

--- Backend/controller/admin.controller.js
+++ Backend/controller/admin.controller.js
@@ -1,1 +1,30 @@
-console.log("test")
+var product = require("../model/product.model");
+var mongoose = require('mongoose');
+
+
+exports.getAllProducts =  function(req,res,next){
+    product.find(function(err,Products){
+        if(err)
+        return next(err);
+        res.json(Products);
+    });
+}
+
+exports.addItem = function(req,res,next){
+    product.create(req.body, function(err,product){
+        if(err) return next(err);
+        res.json(product);
+    })
+}
+exports.deleteItemById = function(req,res,next){
+    product.findByIdAndRemove(req.params.id,req.body,function(err,prod){
+        if(err) return next(err);
+        res.json(prod);
+    });
+}
+exports.updateItemById = function(req,res,next){
+    product.updateItemById(req.params.id,req.body,function(err,prod){
+        if(err) return next(err);
+        res.json(prod);
+    });
+}
--- Backend/router/admin.router.js
+++ Backend/router/admin.router.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
+let adminController  = require("../controller/admin.controller");
+
+// get products from database
+router.get('/', adminController.getAllProducts);
+//   add new prodcuts into database
+router.post('/addItem', adminController.addItem);
+// delete a product
+router.delete('/:id',adminController.deleteItemById);
+//update a product
+router.put("/:id",adminController.updateItemById);
+
+
+
 
 
 
