0866e91fd2ed521755f5f71f9dd955a9aef890cc
Priyam Das <12priyamdas34@gmail.com>
2021-09-16 04:06:09 -0400

    L5 unlock user functionality added

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -1,20 +1,21 @@
-let mongoose= require("mongoose");
-mongoose.Promise= global.Promise;
+let mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 
-let userSchema= mongoose.Schema({
-   userName:String,
-   pass:String,
-   fname:String,
-   lname:String,
-   email:String,
-   addr:String,
-   dob:Date,
-   fund:Number,
-   ticket:Boolean,
-   loginAttempt:Number,
-   phoneNum:String
+let userSchema = mongoose.Schema({
+   userName: String,
+   pass: String,
+   fname: String,
+   lname: String,
+   email: String,
+   addr: String,
+   dob: Date,
+   fund: Number,
+   ticket: Boolean,
+   loginAttempt: Number,
+   phoneNum: String,
+   isLocked: Boolean
 });
 
-let UserModel= mongoose.model(" ",userSchema, "usersTable");
+let UserModel = mongoose.model(" ", userSchema, "usersTable");
 
-module.exports= UserModel;
+module.exports = UserModel;
--- Backend/router/user.router.js
+++ Backend/router/user.router.js
@@ -2,7 +2,7 @@ let express = require("express");
 let router = express.Router();
 let userController = require("../controller/user.controller");
 
-router.post("/signIn",userController.signIn);
-router.post("/signUp",userController.signUp);
+router.post("/signIn", userController.signIn);
+router.post("/signUp", userController.signUp);
 
-module.exports=router;
+module.exports = router;
